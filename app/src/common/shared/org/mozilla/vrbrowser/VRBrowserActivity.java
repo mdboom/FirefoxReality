3acb43be224044c6418b5a1cf8e5681b6c02bad9
Manuel Martin <manmartin@mozilla.com>
2020-02-04 12:15:35 +0100

    Tray ViewModel

--- app/src/common/shared/org/mozilla/vrbrowser/VRBrowserActivity.java
+++ app/src/common/shared/org/mozilla/vrbrowser/VRBrowserActivity.java
@@ -1383,13 +1383,6 @@ public class VRBrowserActivity extends PlatformActivity implements WidgetManager
     }
 
     @Override
-    public void setTrayVisible(boolean visible) {
-        if (mTray != null && !mTray.isReleased()) {
-            mTray.setTrayVisible(visible);
-        }
-    }
-
-    @Override
     public void setControllersVisible(final boolean aVisible) {
         queueRunnable(() -> setControllersVisibleNative(aVisible));
     }
